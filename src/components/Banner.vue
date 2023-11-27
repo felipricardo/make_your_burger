1bf41508eb8d12c1b0240ee888909243ab1a6875
felipricardo <felipebarbosaok@gmail.com>
2023-11-27 16:59:01 -0300

    up

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,13 +1,20 @@
 <template>
-    <p>burger banner</p>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
 </template>
 
 <script>
 export default {
-
+    name: "Banner"
 }
 </script>
 
 <style scoped>
+
+    #main-banner {
+        background-image: url ('/img/burger.jpg');
+        background-position: 0 - 250px;
+    }
 
 </style>
